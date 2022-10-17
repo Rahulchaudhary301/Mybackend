05d7fa481dc1b6db3a84efc4a53dda2cb438d8ca
Rahulchaudhary301 <rahulchaudhary301@gmail.com>
2022-10-17 21:36:17 +0530

    modified By Rahul Chaudhary

--- src/routes/route.js
+++ src/routes/route.js
@@ -38,6 +38,90 @@ router.get('/student-details/:name', function(req, res){
     let studentName = requestParams.name
     console.log('Name of the student is ', studentName)
     res.send('Dummy response')
+
+   
+})
+
+///////////////////////////////////////////////////////Problem 1 /////////////////////////////////////////////////////////////////
+
+router.get('/movie',function (req , res){
+  let movies=['Rang de basanti','The shinig','Lord of the rings','Batman begeain']
+  res.send(movies)
+})
+ 
+////////////////////////////////////////////////////Problem 2 /////////////////////////////////////////////////////////////////////
+router.get('/movie/:index',function (req , res){
+  let movie=['Rang de basanti','The shinig','Lord of the rings','Batman begeain']
+  let index=req.params.index
+  res.send(movie[index])
+})
+
+////////////////////////////////////////////////////Problem 3 /////////////////////////////////////////////////////////////////////
+ 
+router.get('/movie/:index',function (req , res){
+  let movie=['Rang de basanti','The shinig','Lord of the rings','Batman begeain']
+  let index=req.params.index;
+  if(index > movie.length){
+    res.send('Please inter Valid Index')
+  }
+  else{
+    res.send(movie[index])
+  }
+})
+/////////////////////////////////////////////////Problem 4 //////////////////////////////////////////////////////////////////////////
+
+router.get('/films', function(req,res){
+  let filmName=[
+    {
+      "id":1,
+      "Name":"The Shining"
+    },
+    {
+      "id":2,
+      "Name":"Incendies"
+    },
+    {
+      "id":3,
+      "Name":"Rang de Basanti"
+    },
+    {
+      "id":4,
+      "Name":"Finding Nemo"
+    }
+  ]
+  res.send(filmName)
+})
+
+//////////////////////////////////////////////////////////Problem 5///////////////////////////////////////////////////////////////////
+
+router.get('/films/:filmId', function(req,res){
+  let filmName=[
+    {
+      "id":1,
+      "Name":"The Shining"
+    },
+    {
+      "id":2,
+      "Name":"Incendies"
+    },
+    {
+      "id":3,
+      "Name":"Rang de Basanti"
+    },
+    {
+      "id":4,
+      "Name":"Finding Nemo"
+    }
+  ]
+
+
+  const filmId=req.params.filmId
+  if(filmId>filmName.length){
+    res.send('No movie exits with this Id')
+  }
+  else{
+    res.send(filmName[filmId])
+  }
 })
 
 module.exports = router;
