6d3a6dd23ea84ab7790ecfcc3dd9fe2436ead341
Rahul Chaudhary <rahulchaudhary301@gmail.com>
2022-10-26 21:32:33 +0530

    solution of mongoDb 2

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,72 @@
+const { query } = require("express")
 const UserModel= require("../models/userModel")
 
-const createUser= async function (req, res) {
+// const createUser= async function (req, res) {
+//     let data= req.body
+//     let savedData= await UserModel.create(data)
+//     res.send({msg: savedData})
+// }
+
+// const getUsersData= async function (req, res) {
+//     let allUsers= await UserModel.find()
+//     res.send({msg: allUsers})
+// }
+
+/////////////////////////////////////////////////////Assinment///////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////Assinment 1 //////////////////////////////////////////////////////////////////////
+
+const createBooks= async function (req, res) {
     let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
+    let booksCollection= await UserModel.bookmodel.create(data)
+    res.send({msg: booksCollection})
 }
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
-}
+/////////////////////////////////////////////////Assinment 2////////////////////////////////////////////////////////////////////////////////
+const getBooklist= async function (req, res) {
+        let allBooks= await UserModel.bookmodel.find().select({bookName:1, authorName:1, _id:0})
+        res.send({msg: allBooks})
+     }
 
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
+
+/////////////////////////////////////////////////////////Assinment 3///////////////////////////////////////////////////////////////////////
+
+     const getBooksInYear= async function (req, res) {
+        let year=req.query.year
+        let allBooks= await UserModel.bookmodel.find({ year:year})
+        res.send({msg: allBooks})
+     }
+  
+     ///////////////////////////////////////////////////////////Assinment4 /////////////////////////////////////////////////////////////////////
+     const getParticularBooks= async function (req, res) {
+        let body=req.body;
+        let allBooks= await UserModel.bookmodel.find(body)
+        res.send({msg: allBooks})
+
+     }
+
+     //////////////////////////////////////////////////Assinment 5////////////////////////////////////////////////////////////////////
+
+     const getXINRBooks= async function (req, res) {
+        let allBooks= await UserModel.bookmodel.find({"prices.indianPrice":{$in:[100,200,500]}})
+        res.send({msg: allBooks})
+     }
+
+      ////////////////////////////////////////Assinment 6/////////////////////////////////////////////////////////////////////////////////
+
+     const getRandomBooks= async function (req, res) {
+        let allBooks= await UserModel.bookmodel.find({stockAvailable:true, totalPage:{$gt:500}})
+        res.send({msg: allBooks})
+     }
+
+     
+module.exports.getRandomBooks=getRandomBooks
+module.exports.getXINRBooks=getXINRBooks
+module.exports.getParticularBooks=getParticularBooks
+module.exports.getBooksInYear=getBooksInYear
+module.exports.createBooks=createBooks
+
+module.exports.getBooklist=getBooklist
+//module.exports.createUser= createUser
+//module.exports.getUsersData= getUsersData
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,31 +1,54 @@
-const mongoose = require('mongoose');
+ const mongoose = require('mongoose')
+
+// const userSchema = new mongoose.Schema( {
+//     firstName: String,
+//     lastName: String,
+//     mobile: {
+//         type: String,
+//         unique: true,
+//         required: true
+//     },
+//     emailId: String,
+//     gender: {
+//         type: String,
+//         enum: ["male", "female", "LGBTQ"] //"falana" will give an error
+//     },
+//     age: Number,
+//     // isIndian: Boolean,
+//     // parentsInfo: {
+//     //     motherName: String,
+//     //     fatherName: String,
+//     //     siblingName: String
+//     // },
+//     // cars: [ String  ]
+// }, { timestamps: true });
+
+// module.exports = mongoose.model('User', userSchema) //users
+
+/////////////////////////////////////////////Assinment/////////////////////////////////////////////////////////////////////////////////////
+
 
 const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
+    bookName:{
         type: String,
-        unique: true,
-        required: true
+        require: true,
     },
-    emailId: String,
-    gender: {
-        type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
+    prices: {
+        types:String,
+        indianPrice: String,
+        europePrice: String
     },
-    age: Number,
-    // isIndian: Boolean,
-    // parentsInfo: {
-    //     motherName: String,
-    //     fatherName: String,
-    //     siblingName: String
-    // },
-    // cars: [ String  ]
+    year:{
+        type:Number,
+        default:2021
+    },
+    tag:[String],
+    authorName:String,
+    totalPage:Number,
+    stockAvailable:Boolean,
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema) //users
-
-
+module.exports.bookmodel = mongoose.model('BooksList', userSchema)
 
 // String, Number
 // Boolean, Object/json, array
