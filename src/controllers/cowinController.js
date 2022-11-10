9bb4d85a3b3273e3f33b14d032acd54a80e511cc
Rahul Chaudhary <rahulchaudhary301@gmail.com>
2022-11-10 23:55:11 +0530

    Solution of Axios based question

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -9,7 +9,6 @@ let getStates = async function (req, res) {
             url: 'https://cdn-api.co-vin.in/api/v2/admin/location/states'
         }
         let result = await axios(options);
-        console.log(result)
         let data = result.data
         res.status(200).send({ msg: data, status: true })
     }
@@ -69,7 +68,6 @@ let getOtp = async function (req, res) {
         }
 
         let result = await axios(options)
-        console.log(result.data)
         res.status(200).send({ msg: result.data })
     }
     catch (err) {
@@ -79,7 +77,87 @@ let getOtp = async function (req, res) {
 }
 
 
+
+
+
+////////////////////////////////////////////////////Assinment-1///////////////////////////////////////////////////////////////////////
+
+const vassinationAbilityByDistrictId= async function(req,res){
+    try{
+        let id=req.query.district_Id
+        let date=req.query.date
+        let options={
+            method:'get',
+            url:`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${id}&date=${date}`
+        }
+        let result= await axios(options)
+        let data=result.data
+        res.status(200).send({msg:data})
+    }
+    catch(err){
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+
+
+
+
+/////////////////////////////////////////////////////Assinment-2////////////////////////////////////////////////////////////////////
+
+
+  const weather=async function(req,res){
+    try{
+        let arr1=[]
+        let arr=["Bengaluru", "Mumbai","Patna","Delhi","Kolkata","Chennai","London","moscow"]
+        for(let i=0;i<arr.length;i++){
+            let city=arr[i]
+            let options={
+            method:"get",
+            url:`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ec92c65ef4c828f8298b23228b620970`
+         }
+         let result= await axios(options)
+         let data=result.data
+         let {name,main:{temp}}=data
+         //let save=`City: ${name} , temp: ${m}`
+          arr1.push({name,temp})
+           
+        }
+        arr1.sort((a,b)=>{
+            return a.temp - b.temp
+         })
+         res.status(200).send({msg:arr1})
+    }
+    catch(err){
+        res.status(400).send({msg:err.message})
+    }
+  }
+
+
+////////////////////////////////////////////////////Assinment3////////////////////////////////////////////////////////////////////////////
+
+
+ const mems=async function(req,res){
+    try{
+        let {template_id,text0,text1,username,password}=req.query
+        let options={
+            method:"get",
+            url:`https://api.imgflip.com/caption_image?template_id=${template_id}&text0=${text0}&text1=${text1}&username=${username}&password=${password}`
+        }
+        let result= await axios(options)
+        res.status(200).send({msg:result.data})
+    }
+    
+    catch(err){
+        res.status(400).send({msg:err.message})
+    }
+ }
+ 
+ 
+module.exports.mems=mems
+module.exports.weather=weather
 module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
 module.exports.getOtp = getOtp
+module.exports.vassinationAbilityByDistrictId=vassinationAbilityByDistrictId
--- src/routes/route.js
+++ src/routes/route.js
@@ -11,9 +11,11 @@ router.get("/test-me", function (req, res) {
 
 router.get("/cowin/states", CowinController.getStates)
 router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
+router.get('/check-vassination-ability',CowinController.vassinationAbilityByDistrictId)
 router.get("/cowin/getByPin", CowinController.getByPin)
-
 router.post("/cowin/getOtp", CowinController.getOtp)
+router.get('/get_weather-condition', CowinController.weather)
+router.post('/creat-mems', CowinController.mems)
 
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
 
